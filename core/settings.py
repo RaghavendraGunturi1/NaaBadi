a8a635c0c7f4a7b4937f8614cad58556b7438673
Venkata Sri Raghavendra Gunturi <162595474+RaghavendraGunturi1@users.noreply.github.com>
2025-12-18 14:22:06 +0530

    Reorder import statements in settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
+import os
+import dj_database_url # You likely added this earlier, keep it.
 from pathlib import Path
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
