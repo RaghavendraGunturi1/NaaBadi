2813c5f16a70759fd246788a90db03d0779db62f
RaghavendraGunturi1 <raghavendra@newowl.co>
2025-12-19 13:07:04 +0530

    n

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import home
+from website import views  # Import your views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('programs/', views.programs, name='programs'),
+    path('resources/', views.resources, name='resources'),
+    path('join/', views.contact, name='contact'),
 ]
 
-# This part tells Django to serve uploaded images during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- website/views.py
+++ website/views.py
@@ -1,44 +1,34 @@
-from django.shortcuts import render
-from django.db.models import Sum
-from .models import School, Program
-
 from django.shortcuts import render, redirect
 from django.db.models import Sum
-from .models import School, Program
-from .forms import VolunteerForm # Import the new form
-
-from django.shortcuts import render, redirect
-from django.db.models import Sum
-from django.contrib import messages  # <--- NEW: For pop-up messages
-from .models import School, Program, Volunteer, SuccessStory
+from django.contrib import messages
+from .models import School, Program, Volunteer
 from .forms import VolunteerForm
 
 def home(request):
-    # --- FORM HANDLING ---
+    total_schools = School.objects.count()
+    total_students = School.objects.aggregate(Sum('students_impacted'))['students_impacted__sum'] or 0
+    return render(request, 'website/index.html', {
+        'schools_count': total_schools,
+        'students_count': total_students
+    })
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def programs(request):
+    programs = Program.objects.all()
+    return render(request, 'website/programs.html', {'programs': programs})
+
+def resources(request):
+    return render(request, 'website/resources.html')
+
+def contact(request):
     if request.method == 'POST':
         form = VolunteerForm(request.POST)
         if form.is_valid():
             form.save()
-            # Add a success message that will show up on the frontend
             messages.success(request, "Welcome to the Squad! We will contact you soon.")
-            return redirect('home')
+            return redirect('contact')
     else:
         form = VolunteerForm()
-
-    # --- DATA FETCHING ---
-    programs = Program.objects.all()
-    # Fetch stories (Newest first)
-    stories = SuccessStory.objects.all().order_by('-id')[:3] 
-    
-    total_schools = School.objects.count()
-    total_students = School.objects.aggregate(Sum('students_impacted'))['students_impacted__sum'] or 0
-
-    context = {
-        'form': form,
-        'programs': programs,
-        'stories': stories, # <--- Sending stories to HTML
-        'schools_count': total_schools,
-        'students_count': total_students,
-    }
-    
-    return render(request, 'website/index.html', context)
+    return render(request, 'website/contact.html', {'form': form})
